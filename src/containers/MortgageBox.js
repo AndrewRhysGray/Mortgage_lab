9d22f1b9f428c1d5cd16ae5401622146e53b223b
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-03-04 10:29:43 +0000

    Test successful

---
+++ src/containers/MortgageBox.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+
+class MortgageBox extends Component{
+
+
+
+  render (){
+    return (
+      <article>
+      <h1> Test! </h1>
+      </article>
+    )
+  }
+
+}
+
+
+
+
+
+export default MortgageBox
