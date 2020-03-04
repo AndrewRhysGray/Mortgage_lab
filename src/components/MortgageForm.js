c103f67f9cc934f0c93c620afe7f4b7f5b8c3f59
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-03-04 11:16:06 +0000

    two errors, rendering successful

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -8,20 +8,35 @@ class MortgageForm extends Component {
       salary: ''
     }
     this.handlePrimarySalaryChange = this.handlePrimarySalaryChange.bind(this)
+    this.handleMortgageSubmit = this.handleMortgageSubmit.bind(this)
+
+
   }
 
   handlePrimarySalaryChange(event){
 this.setState({salary: event.target.value})
   }
 
+  handleMortgageSubmit(event){
+    event.preventDefault()
+    const newMortgage = {
+      salary: this.state.salary
+    }
+    this.props.onMortgageAddition(newMortgage)
+  }
+
   render(){
     return (
-      <form>
+      <form onSubmit={this.handleMortgageSubmit}>
       <input
       type ='text'
       placeholder='Primary salary'
       value={this.state.salary}
       onChange = {this.handlePrimarySalaryChange}
+      />
+      <input
+      type="submit"
+      value="Calculate Mortgage"
       />
       </form>
 
--- src/containers/MortgageBox.js
+++ src/containers/MortgageBox.js
@@ -7,17 +7,31 @@ class MortgageBox extends Component{
 constructor(props) {
   super (props)
   this.state = {
-    mortgages: []
+    mortgages: [
+      {
+        id: 1,
+        salary: 500
+      }
+    ]
   }
+  this.handleMortgageAddition =  this.handleMortgageAddition.bind(this)
+
 }
 
+handleMortgageAddition(mortgageToAdd){
+  mortgageToAdd.id = Date.now()
+  const updatedMortgages = [...this.state.mortgage, mortgageToAdd]
+
+  this.setState({ mortgages: updatedMortgages})
+}
 
 
   render (){
     return (
       <article>
       <h1> Test! </h1>
-      <MortgageForm />
+      <MortgageForm
+      onMortgageAddition = {this.handleMortgageAddition}/>
       <MortgageList mortgages={this.state.mortgages}/>
       </article>
     )
